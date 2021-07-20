18e9413badca6a6df2cf06b0b7a0b10b83ace345
bakaemon <69375806+bakaemon@users.noreply.github.com>
2021-07-20 09:04:55 +0700

    changed embed

--- cogs/music.py
+++ cogs/music.py
@@ -144,12 +144,12 @@ class Song:
 
     def create_embed(self):
         embed = (discord.Embed(title='Now playing',
-                               description='```css\n{0.source.title}\n```'.format(self),
+                               description=self.source.title,
+                               url=self.source.url,
                                color=discord.Color.blurple())
                  .add_field(name='Duration', value=self.source.duration)
                  .add_field(name='Requested by', value=self.requester.mention)
                  .add_field(name='Uploader', value='[{0.source.uploader}]({0.source.uploader_url})'.format(self))
-                 .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
                  .set_thumbnail(url=self.source.thumbnail))
 
         return embed
